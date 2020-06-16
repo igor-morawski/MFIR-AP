ca21e168663a95803f0c9ff6230c19e88a26b750
igor-morawski <igor.morawski@hotmail.com>
2020-06-16 11:58:40 +0800

    compare

--- compare.py
+++ compare.py
@@ -19,6 +19,20 @@ if __name__ == "__main__":
     args = parser.parse_args()
     models = args.input
 
+    # ablation info
+    ablation_dict = {}
+
+    ablation_dict[""] = "No ablation, all models included."
+    ablation_dict["1"] = "Frame number and shift ablation."
+    ablation_dict["A2"] = "View ablation, frame number and shift A."
+    ablation_dict["B2"] = "View ablation, frame number and shift B."
+    ablation_dict["C2"] = "View ablation, frame number and shift C."
+    ablation_dict["D2"] = "View ablation, frame number and shift D."
+
+    ablations = list(ablation_dict.keys())
+    ablations.sort()
+    # /ablation info
+
     reporting_dir = os.path.join("data", "06_reporting", "comparison")
     html_path = os.path.join(reporting_dir, "report.html")
     plots_fps = []
@@ -39,6 +53,16 @@ if __name__ == "__main__":
             d = pickle.load(f)
         models_data_dict[model] = d
         models_data_dict[model]["metrics_dict"]["name"] = model
+        # add ablation *1x -> *2
+        if model[-1] == "1":
+            alias = model[:-1]+"2crl"
+            models_data_dict[alias] = d
+            models_data_dict[alias]["metrics_dict"]["name"] = alias
+    for key in models_data_dict.keys():
+        if key not in models:
+            models.append(key)
+
+    
 
     # ROC and PRC
     rocs, det_rocs, prc, det_prc = [{} for i in range(4)]
@@ -48,7 +72,11 @@ if __name__ == "__main__":
         prc[model] = models_data_dict[model]['plots_dict']['precision_recall_curve']
         det_prc[model] = models_data_dict[model]['plots_dict']['detection_precision_recall_curve']
 
-    loc_args = {'loc': 'lower right'}
+    # plot settings
+    from matplotlib.font_manager import FontProperties
+    fontP = FontProperties()
+    fontP.set_size('small')
+    loc_args = {'loc': (1.05, 0.5), 'ncol': 2, 'prop': fontP}
     plot_args = {'lw': 2}
 
     GUIDELINES_KWARGS = {"color": 'gray', "linestyle": '--'}
@@ -64,65 +92,77 @@ if __name__ == "__main__":
         plt.xlim(0, 1+lim_eps)
         plt.ylim(0, 1+lim_eps)
 
-    plt.title('ROC - prediction')
-    for model in models:
-        d = rocs[model]
-        fpr, tpr = d['fpr'], d['tpr']
-        plt.plot(fpr, tpr, label=model, **plot_args)
-    plt.xlabel('False positive rate')
-    plt.ylabel('True positive rate')
-    plt.legend(**loc_args)
-    set_lims()
-    draw_iden_line()
-    fp = os.path.join(plots_dir, "prediction_roc.png")
-    plt.savefig(fp)
-    plots_fps.append(os.path.relpath(fp, reporting_dir))
-    plt.close()
+    for ablation in ablations:
+        plt.title('ROC - prediction {}'.format("(ablation {})".format(ablation) if ablation else "(all models)"))
+        for model in models:
+            if ablation not in model:
+                continue
+            d = rocs[model]
+            fpr, tpr = d['fpr'], d['tpr']
+            plt.plot(fpr, tpr, label=model+", AUC = {:.2f}".format(models_data_dict[model]["metrics_dict"]["prediction_auc"]), **plot_args)
+        plt.xlabel('False positive rate')
+        plt.ylabel('True positive rate')
+        plt.legend(**loc_args)
+        set_lims()
+        draw_iden_line()
+        fp = os.path.join(plots_dir, "prediction_roc{}.png".format(ablation))
+        plt.savefig(fp, bbox_inches='tight')
+        plots_fps.append(os.path.relpath(fp, reporting_dir))
+        plt.close()
 
-    plt.title('ROC - detection')
-    for model in models:
-        d = det_rocs[model]
-        fpr, tpr = d['fpr'], d['tpr']
-        plt.plot(fpr, tpr, label=model, **plot_args)
-    plt.xlabel('False positive rate')
-    plt.ylabel('True positive rate')
-    plt.legend(**loc_args)
-    set_lims()
-    draw_iden_line()
-    fp = os.path.join(plots_dir, "detection_roc.png")
-    plt.savefig(fp)
-    plots_fps.append(os.path.relpath(fp, reporting_dir))
-    plt.close()
+        plt.title('ROC - detection {}'.format("(ablation {})".format(ablation) if ablation else "(all models)"))
+        for model in models:
+            if ablation not in model:
+                continue
+            d = det_rocs[model]
+            fpr, tpr = d['fpr'], d['tpr']
+            plt.plot(fpr, tpr, label=model+", AUC = {:.2f}".format(models_data_dict[model]["metrics_dict"]["detection_auc"]), **plot_args)
+        plt.xlabel('False positive rate')
+        plt.ylabel('True positive rate')
+        plt.legend(**loc_args)
+        set_lims()
+        draw_iden_line()
+        fp = os.path.join(plots_dir, "detection_roc{}.png".format(ablation))
+        plt.savefig(fp, bbox_inches='tight')
+        plots_fps.append(os.path.relpath(fp, reporting_dir))
+        plt.close()
 
-    plt.title('Precision-recall curve - prediction')
-    for model in models:
-        d = prc[model]
-        precision, recall = d['precision'], d['recall']
-        plt.plot(recall, precision, label=model, **plot_args)
-    plt.xlabel('Recall')
-    plt.ylabel('Precision')
-    plt.legend(**loc_args)
-    set_lims()
-    draw_hline()
-    fp = os.path.join(plots_dir, "prediction_prc.png")
-    plt.savefig(fp)
-    plots_fps.append(os.path.relpath(fp, reporting_dir))
-    plt.close()
+        plt.title('Precision-recall curve - prediction {}'.format("(ablation {})".format(ablation) if ablation else "(all models)"))
+        for model in models:
+            if ablation not in model:
+                continue
+            d = prc[model]
+            precision, recall = d['precision'], d['recall']
+            plt.plot(recall, precision, label=model, **plot_args)
+        plt.xlabel('Recall')
+        plt.ylabel('Precision')
+        plt.legend(**loc_args)
+        set_lims()
+        draw_hline()
+        fp = os.path.join(plots_dir, "prediction_prc{}.png".format(ablation))
+        plt.savefig(fp, bbox_inches='tight')
+        plots_fps.append(os.path.relpath(fp, reporting_dir))
+        plt.close()
 
-    plt.title('Precision-recall curve - detection')
-    for model in models:
-        d = det_prc[model]
-        precision, recall = d['precision'], d['recall']
-        plt.plot(recall, precision, label=model, **plot_args)
-    plt.xlabel('Recall')
-    plt.ylabel('Precision')
-    plt.legend(**loc_args)
-    set_lims()
-    draw_hline()
-    fp = os.path.join(plots_dir, "detection_prc.png")
-    plt.savefig(fp)
-    plots_fps.append(os.path.relpath(fp, reporting_dir))
-    plt.close()
+        plt.title('Precision-recall curve - detection {}'.format("(ablation {})".format(ablation) if ablation else "(all models)"))
+        for model in models:
+            if ablation not in model:
+                continue
+            d = det_prc[model]
+            precision, recall = d['precision'], d['recall']
+            plt.plot(recall, precision, label=model, **plot_args)
+        plt.xlabel('Recall')
+        plt.ylabel('Precision')
+        plt.legend(**loc_args)
+        set_lims()
+        draw_hline()
+        fp = os.path.join(plots_dir, "detection_prc{}.png".format(ablation))
+        plt.savefig(fp, bbox_inches='tight')
+        plots_fps.append(os.path.relpath(fp, reporting_dir))
+        plt.close()
+    
+    plots_fps.sort()
+    # /plots
 
     report_data = {}
     plots_html = str(
@@ -135,10 +175,11 @@ if __name__ == "__main__":
         ['''<li> <a href="{}">{}</a></li>'''.format(l, m) for l, m in zip(links_fps, models)]))
     report_data["links_html"] = links_html
 
-    sortable_header_keys = ['name', 'ATTA', 'APT', 'precision', 'recall',
+    sortable_header_keys = ['name', 'APT', 'ATTA',  'precision', 'recall',
                             'accuracy', 'detection_precision', 'detection_recall', 'detection_accuracy', "prediction_auc", "detection_auc"]
     row_pattern = "<tr>{}</tr>"
-    cell_pattern = '<td width="{w}%">{brackets}</td>'.format(w=100//len(sortable_header_keys), brackets="{}")
+    cell_pattern = '<td width="{w}%">{brackets}</td>'.format(
+        w=100//len(sortable_header_keys), brackets="{}")
     metric_cell_pattern = cell_pattern.format("{:.3f}")
     header_cell_pattern = "<th>{}</th>"
 
@@ -147,12 +188,52 @@ if __name__ == "__main__":
             return metric_cell_pattern.format(x)
         except ValueError:
             return cell_pattern.format(x)
-    sortable_header = "".join(header_cell_pattern.format(h) for h in sortable_header_keys)
+    sortable_header = "".join(header_cell_pattern.format(h)
+                              for h in sortable_header_keys)
     sortable_table_content = "".join([(row_pattern.format("".join(cell_formatter(
         models_data_dict[model]["metrics_dict"][key]) for key in sortable_header_keys))) for model in models])
 
     sortable_header = sortable_header.replace("_", " ")
-    
+
+    terminology_html = '''
+    <hr>
+    <h2>Appendix A. Metrics and terms.</h2>
+    <h3>General</h3>
+    <ul>
+        <li>Precision = TP/(TP+FP)</li>
+        <li>Recall = TP/(TP+FN)</li>
+        <li>Accuracy = (TP+TN)/(TP+TN+FP+FN)</li>
+        <li>ROC - receiver operating characteristic curve.</li>
+        <li>AUC - area under curve - is equal to the probability that a classifier will rank a randomly chosen positive instance higher than a randomly chosen negative one.</li>
+    </ul>
+    <h3>Research-specific</h3>
+    <ul>
+        <li>APT - prediction time, always a positive number.</li>
+        <li>ATTA - prediction and detection time, any real number.</li>
+    </ul>
+    '''
+    report_data["terminology_html"] = terminology_html
+
+    ablations_list_html = "".join("<li>{} - {}</li>".format(ablation, ablation_dict[ablation]) if ablation else "" for ablation in ablations)
+    dataset_and_ablations_html = '''
+    <h2>Appendix B. Dataset and ablation variations.</h2>
+    <ul>
+        <li>Training set subjects: 9</li>
+            <ul>  
+                <li> 30 positive samples (each),</li>
+                <li> negative samples augmented by extracting patches in temporal dimension, positive:negative ratio balanced during training.
+            </ul>
+        <li>Testing set subjects: 3</li>
+            <ul>  
+                <li> 30 positive samples (each), around 1-1.5 min. long,</li>
+                <li> 30 negative smaples (each), around 1-1.5 min. long.
+            </ul>
+    </ul>
+    <h3>Ablations</h3>
+    {ablations_list_html}
+    '''.format(**{"ablations_list_html": ablations_list_html})
+    report_data["dataset_and_ablations_html"] = dataset_and_ablations_html
+
     sortable_html = '''
     <table class="sortable" width="100%">
     <thead>
@@ -162,7 +243,7 @@ if __name__ == "__main__":
     {sortable_table_content}
     </tbody>
     </table>
-    '''.format(**{"sortable_header":sortable_header, "sortable_table_content":sortable_table_content})
+    '''.format(**{"sortable_header": sortable_header, "sortable_table_content": sortable_table_content})
     report_data["sortable_html"] = sortable_html
 
     report_html = '''
@@ -230,7 +311,8 @@ if __name__ == "__main__":
             {links_html}
             <br>
             {plots_html}
-            
+            {terminology_html}
+            {dataset_and_ablations_html}
         </body>
     </html> 
     '''.format(**report_data)
